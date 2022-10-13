f73c8ea188a636960e7643da4e6fa8a2255901b8
Ildus Khabirov <habirov-91@yandex.ru>
2022-10-13 21:31:43 +0500

    Верстка страниц и шаблонов

---
+++ src/pages/error-page/error-page.js
@@ -0,0 +1,2 @@
+// language=hbs
+export default `{{> error-notification Code="404" Description="Не туда попали"}}`;
---
+++ src/pages/error-page/index.js
@@ -0,0 +1,5 @@
+import Handlebars from 'handlebars';
+import template from './error-page';
+import './style.scss';
+
+Handlebars.registerPartial('error-page', template);
